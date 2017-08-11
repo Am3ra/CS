8f896bcd090c6975ba23018a4209ba1ac2b48b7a
am3ra <jp2kk2@gmail.com>
2017-08-11 12:47:55 -0500

    Clase 2 FDP

--- CS101/ejercicios/Ejercicios02/6.c
+++ CS101/ejercicios/Ejercicios02/6.c
@@ -3,11 +3,11 @@
 
 int main() {
   float x = 13.36;
-  printf("Dolares    Pesos");
-  printf("****************");
+  printf("Dolares    Pesos\n");
+  printf("****************\n");
   for (  int i = 10; i < 100;) {
     float pesos = x * i;
-    printf("%d             %f",i,pesos );
+    printf("%d             %f\n",i,pesos );
     i += 10;
   }
   return 0;
