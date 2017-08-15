03cb65f0a22807c87f8bd8e5c47dafa808dc8e46
am3ra <jp2kk2@gmail.com>
2017-08-15 14:40:06 -0500

    .

--- CS101/ejercicios/4-2.c
+++ CS101/ejercicios/4-2.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
-
 int a, b , c,m2, total, fabrica, tapas;
-
 int main( ) {
   printf("\n\n\n\n\n\n\n");
-
   printf("Indica la cantidad de tinacos del modeo A :  ");
   scanf("%d",&a);
   printf("Indica la cantidad de tinacos del modeo B :  ");
   scanf("%d",&b);
   printf("Indica la cantidad de tinacos del modeo C :  ");
   scanf("%d",&c);
-
   m2 = a * 10 + b * 15 + c* 20;
   tapas = a + b + c;
-
   printf("******************************");
   printf("\nCantidad de :");
   printf("\n     M2 de plastico     : %8d", m2);
@@ -25,7 +20,7 @@ int main( ) {
   printf("\n******************************");
   printf("\nCosto total : %8d", (a*10*50)+(b*15*50)+(c*20*50)+(a+b+c)*20 + a*25+b*35+c*45);
   printf("\nPrecio final: %8d", a*760 +b*1130+c*1490);
-
   printf("\n\n\n\n\n\n");
+  print(float(a));
   return 0;
 }
---
+++ CS101/ejercicios/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int y = 6;
+
+int main()
+{
+  printf("%f", (float)y);
+}
