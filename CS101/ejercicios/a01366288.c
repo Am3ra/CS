1f634bbca241796bb9ab7821f09f637abafbaa4e
am3ra <jp2kk2@gmail.com>
2017-08-29 12:33:45 -0500

    examen mañón

--- CS101/ejercicios/a01366288.c
+++ CS101/ejercicios/a01366288.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 float v1, v2, v3, desc, total, totalc;
-char mejor[12];
+int mejor;
 
 int main(int argc, char const *argv[]) {
 
@@ -20,98 +20,109 @@ int main(int argc, char const *argv[]) {
 
   if (v1 <= 10000){
     desc = 0;
-    printf("Comisi%cn del vendedor 1 en porcentaje: %2.0f \n", 162, desc*100);
-    printf("Comisi%cn del vendedor 1 en pesos     : %2.0f \n", 162, desc*v1);
+    printf("Comisión del vendedor 1 en porcentaje: %8.0f \n", desc*100);
+    printf("Comisión del vendedor 1 en pesos     : %8.0f \n", desc*v1);
     totalc = totalc + desc * v1;
 
   } else if (v1 > 10000 && v1 <= 50000){
     desc = 0.02;
-    printf("Comisi%cn del vendedor 1 en porcentaje: %2.0f \n", 162, desc*100);
-    printf("Comisi%cn del vendedor 1 en pesos     : %2.0f \n", 162, desc*v1);
+    printf("Comisión del vendedor 1 en porcentaje: %8.0f \n", desc*100);
+    printf("Comisión del vendedor 1 en pesos     : %8.0f \n", desc*v1);
     totalc = totalc + desc * v1;
 
 
   } else if (v1 > 50000 && v1 <= 100000){
     desc = 0.03;
-    printf("Comisi%cn del vendedor 1 en porcentaje: %2.0f \n", 162, desc*100);
-    printf("Comisi%cn del vendedor 1 en pesos     : %2.0f \n", 162, desc*v1);
+    printf("Comisión del vendedor 1 en porcentaje: %8.0f \n", desc*100);
+    printf("Comisión del vendedor 1 en pesos     : %8.0f \n", desc*v1);
     totalc = totalc + desc * v1;
 
   } else if (v1 > 100000){
     desc = 0.04;
-    printf("Comisi%cn del vendedor 1 en porcentaje: %2.0f \n", 162, desc*100);
-    printf("Comisi%cn del vendedor 1 en pesos     : %2.0f \n", 162, desc*v1);
+    printf("Comisión del vendedor 1 en porcentaje: %8.0f \n", desc*100);
+    printf("Comisión del vendedor 1 en pesos     : %8.0f \n", desc*v1);
     totalc = totalc + desc * v1;
 
   }
 
   if (v2 <= 10000){
     desc = 0;
-    printf("Comisi%cn del vendedor 1 en porcentaje: %2.0f \n", 162, desc*100);
-    printf("Comisi%cn del vendedor 1 en pesos     : %2.0f \n", 162, desc*v2);
+    printf("Comisión del vendedor 1 en porcentaje: %8.0f \n", desc*100);
+    printf("Comisión del vendedor 1 en pesos     : %8.0f \n", desc*v2);
     totalc = totalc + desc * v2;
 
   } else if (v2 > 10000 && v2 <= 50000){
     desc = 0.02;
-    printf("Comisi%cn del vendedor 1 en porcentaje: %2.0f \n", 162, desc*100);
-    printf("Comisi%cn del vendedor 1 en pesos     : %2.0f \n", 162, desc*v2);
+    printf("Comisión del vendedor 1 en porcentaje: %8.0f \n", desc*100);
+    printf("Comisión del vendedor 1 en pesos     : %8.0f \n", desc*v2);
     totalc = totalc + desc * v2;
 
 
   } else if (v2 > 50000 && v2 <= 100000){
     desc = 0.03;
-    printf("Comisi%cn del vendedor 1 en porcentaje: %2.0f \n", 162, desc*100);
-    printf("Comisi%cn del vendedor 1 en pesos     : %2.0f \n", 162, desc*v2);
+    printf("Comisión del vendedor 1 en porcentaje: %8.0f \n", desc*100);
+    printf("Comisión del vendedor 1 en pesos     : %8.0f \n", desc*v2);
     totalc = totalc + desc * v2;
 
   } else if (v2 > 100000){
     desc = 0.04;
-    printf("Comisi%cn del vendedor 1 en porcentaje: %2.0f \n", 162, desc*100);
-    printf("Comisi%cn del vendedor 1 en pesos     : %2.0f \n", 162, desc*v2);
+    printf("Comisión del vendedor 1 en porcentaje: %8.0f \n", desc*100);
+    printf("Comisión del vendedor 1 en pesos     : %8.0f \n", desc*v2);
     totalc = totalc + desc * v2;
 
   }
 
   if (v3 <= 10000){
     desc = 0;
-    printf("Comisi%cn del vendedor 1 en porcentaje: %2.0f \n", 162, desc*100);
-    printf("Comisi%cn del vendedor 1 en pesos     : %2.0f \n", 162, desc*v3);
+    printf("Comisión del vendedor 1 en porcentaje: %8.0f \n",  desc*100);
+    printf("Comisión del vendedor 1 en pesos     : %8.0f \n",  desc*v3);
     totalc = totalc + desc * v3;
 
   } else if (v3 > 10000 && v3 <= 50000){
     desc = 0.02;
-    printf("Comisi%cn del vendedor 1 en porcentaje: %2.0f \n", 162, desc*100);
-    printf("Comisi%cn del vendedor 1 en pesos     : %2.0f \n", 162, desc*v3);
+    printf("Comisión del vendedor 1 en porcentaje: %8.0f \n", desc*100);
+    printf("Comisión del vendedor 1 en pesos     : %8.0f \n", desc*v3);
     totalc = totalc + desc * v3;
 
 
   } else if (v3 > 50000 && v3 <= 100000){
     desc = 0.03;
-    printf("Comisi%cn del vendedor 1 en porcentaje: %2.0f \n", 162, desc*100);
-    printf("Comisi%cn del vendedor 1 en pesos     : %2.0f \n", 162, desc*v3);
+    printf("Comisión del vendedor 1 en porcentaje: %8.0f \n", desc*100);
+    printf("Comisión del vendedor 1 en pesos     : %8.0f \n", desc*v3);
     totalc = totalc + desc * v3;
 
   } else if (v3 > 100000){
     desc = 0.04;
-    printf("Comisi%cn del vendedor 1 en porcentaje: %2.0f \n", 162, desc*100);
-    printf("Comisi%cn del vendedor 1 en pesos     : %2.0f \n", 162, desc*v3);
+    printf("Comisión del vendedor 1 en porcentaje: %8.0f \n", desc*100);
+    printf("Comisión del vendedor 1 en pesos     : %8.0f \n", desc*v3);
     totalc = totalc + desc * v3;
-
   }
 
-  mejor = "Vendedor 1";
+  mejor = 1;
   if (v2>v1){
-    mejor = "Vendedor 2";
-    if (v3>mejor){
-      mejor = "Vendedor 3";
+    mejor = 2;
+    if (v3>v2){
+      mejor = 3;
     }
   }
-  if (v3>mejor){
-    mejor = "vendedor 3";
+  if (v3>v1){
+    mejor = 3;
   }
 
-  printf("Total de ventas                       : %.0f \n",total);
-  printf("Total de comiones                     : %.0f \n",totalc);
+  printf("Total de ventas                      : %8.0f \n",total);
+  printf("Total de comisones                    : %8.0f \n",totalc);
+
+  switch (mejor) {
+    case 1:
+      printf("Vendedor que más vendió              : Vendedor 1 \n");
+      break;
+    case 2:
+      printf("Vendedor que más vendió              : Vendedor 2 \n");
+      break;
+    case 3:
+      printf("Vendedor que más vendió              : Vendedor 3 \n");
+      break;
+  }
 
 
   return 0;
