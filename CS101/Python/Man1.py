81d37820e33f23a8fb1417861f121aeb89c3b633
am3ra <jp2kk2@gmail.com>
2017-10-02 07:55:54 -0500

    .

---
+++ CS101/Python/Man1.py
@@ -0,0 +1,8 @@
+y=1
+x=1
+
+while x-y > 10 :
+    y=1
+    x=1
+    while x<1 and x>1000:
+        x=
---
+++ CS101/ejercicios/lineas_gotoxy.cpp
@@ -0,0 +1,33 @@
+#if defined(_WIN32) || defined(WIN32) // Windows-way ~
+   #define WIN32_LEAN_AND_MEAN // para no incluir cosas innecesarias en windows.h
+   #include <Windows.h>
+   #define gotoxy(x,y) {COORD a;a.X = x; a.Y = y;SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),a);}
+#else // Linux-Way 😊
+   #include <ncurses.h>
+   #define gotoxy(x,y) move(x,y)
+#endif
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+int main(){
+	for(int i=1; i<=90; i++)
+	{
+	gotoxy(i,10);
+	printf("*");
+	usleep(100);
+	gotoxy(i,10);
+	printf(" ");
+	}
+	for(int i=90; i<=90; i--)
+	{
+	gotoxy(i,10);
+	printf("*");
+	usleep(100);
+	gotoxy(i,11);
+	printf(" ");
+	}
+	getche();
+  return 0;
+}
