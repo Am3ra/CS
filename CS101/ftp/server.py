cc458fd77ec28ec95c5a08d26d88ba69f35d1c36
am3ra <jp2kk2@gmail.com>
2018-04-16 07:11:34 -0500

    tiny change ftp

--- CS101/ftp/server.py
+++ CS101/ftp/server.py
@@ -6,6 +6,7 @@ import os
 
 FTP_PORT = 21
 LOCAL_IP = "localhost"
+HIGHEST_DIRECTORY = "/Users/macbookpro/Documents/CS/CS101/ftp"
 
 
 class FTPserver():
@@ -27,13 +28,19 @@ class FTPserver():
         '''Close the server.'''
         self.sock.close()
 
+
 class FTPthread():
     '''Create thread of server to attend user requests in parallel'''
+
     def __init__(self):
         '''initialization of class'''
 
+
 if __name__ == "__main__":
     FTP = FTPserver()
+    HIGHEST_DIRECTORY = os.path.normpath(
+        input("Please write highest directory:"))
+    print(HIGHEST_DIRECTORY)
     FTP.start()
     print("FTP Server currently running")
     input('Enter to end...\n')
