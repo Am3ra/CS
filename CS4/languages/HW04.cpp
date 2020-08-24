bf3b3fa99c9470ea75343729f084fe15ba51f526
am3ra <jp2kk2@gmail.com>
2020-08-24 17:01:36 -0500

    unknown

---
+++ CS4/languages/HW04.cpp
@@ -0,0 +1,42 @@
+/*
+	Complete the following functions and submit your file to Canvas.
+
+	========= IMPORTANT ==========
+	Change the name of the file so that it contains your student ID or name.
+	Do not change the names of the functions. The system will specifically look for the names used in this file. Changing the name of a function will automatically make you lose the points for that function.
+	Do not change the number of arguments in the functions. Changing the number of arguments of a function will automatically make you lose the points for that function.
+	Make sure that nothing additional is printed as the result of running this file. Avoid printing names, student IDs, or any other messages. If you print anything else, the system will wrongly grade your solutions.
+	If your file does not compile, your grade will be zero. Then, submit only code that works.
+
+	To compile, run: "g++ HW05.cpp -std=c++11 -o HW05"
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int sum(vector<int> v)
+{
+	cout << "Not yet implemented.";
+	return 0;
+}
+
+int fibonacci(int n)
+{
+	cout << "Not yet implemented.";
+	return 0;
+}
+
+int toBinary(int n)
+{
+	cout << "Not yet implemented.";
+	return 0;	
+}
+
+int main()
+{
+	/*
+		Test your codes here.
+	*/	
+}
