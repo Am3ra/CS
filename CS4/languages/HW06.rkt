b3ea7162ccc3f6f60379fa8a6e963a9bccbb5a08
am3ra <jp2kk2@gmail.com>
2020-09-11 22:05:08 -0500

    finish hw 06 languages

--- CS4/languages/HW06.rkt
+++ CS4/languages/HW06.rkt
@@ -13,10 +13,15 @@
 ;; === sum ===
 
 (define (sum matrix)
-	(apply (apply +))
+	(apply + (map  (lambda (row)
+		(apply + row)	
+		)
+		matrix
+	))
 )
 
 (display "=== sum ===\n")
+;;; (sum '((1 2 3))) ;; 45
 (sum '((1 2 3) (4 5 6) (7 8 9))) ;; 45
 (sum '((1 2) (30 40) (5 6) (70 80))) ;; 234
 (sum '((8 9 5 6 7) (3 4 5 4 2))) ;; 53
@@ -24,7 +29,11 @@
 ;; === complete? ===
 
 (define (complete? graph)
-	(display "Not yet implemented\n")
+	(let ([len (length graph)])
+		(andmap (lambda (row)
+			(= (length row) len)
+		) graph)
+	)
 )
 
 (display "=== complete? ===\n")
@@ -33,14 +42,40 @@
 (complete? '( (a c) (b a) (c a))) ;; #f
 
 ;; === msort ===
+(define (mlists x y)
+	(cond
+		[(null? x) y]
+		[(null? y) x]
+		[(> (car x) (car y))
+			(cons (car y) (mlists (cdr y) x))
+		]
+		[else
+			(cons (car x) (mlists (cdr x) y))
+		]
+	)
+
+)
 
 (define (msort lst)
-	(display "Not yet implemented\n")
+	
+	(cond
+		[(null? lst) lst]
+		[(= (length lst) 1) lst]
+		[(= (length lst) 2) 
+			(mlists (list (car lst)) (cdr lst))
+		]
+		[else
+			 (let ([xs (ceiling (/ (length lst) 2))])
+       			(mlists (msort (take lst xs))
+				(msort (drop lst xs))))
+		]
+	)
 )
 
 (display "=== msort ===\n")
 (msort '()) ;; '()
 (msort '(1)) ;; '(1)
+(msort '(1 2)) ;; '(1)
 (msort '(10 35 8 2.6 4.7 12)) ;; '(2.6 4.7 8 10 12 35)
 (msort '(1 4 7 9 3 4 8 10)) ;; '(1 3 4 4 7 8 9 10)
 
@@ -58,7 +93,20 @@
 )
 
 (define (sold-units id sales)
-  	(display "Not yet implemented\n")  	
+  	(sum (map
+		(lambda (row)
+			(map
+				(lambda (item)
+					(if (= (car item) id)
+						(cadr item)
+						0
+					)
+				)
+				(cdr row)
+			)
+		)
+		sales  
+	))
 )
 
 (display "=== sold-units ===\n")
@@ -70,7 +118,17 @@
 ;; === insert ===
 
 (define (insert x tree)
-	(display "Not yet implemented\n")
+	(cond 
+		[(null? tree)
+			(list x null null)
+		]
+		[(< x (car tree))
+			(list (car tree) (insert x (cadr tree)) (caddr tree))
+		]
+		[else
+			(list (car tree) (cadr tree) (insert x (caddr tree)))
+		]
+	)
 )
 
 (display "=== insert ===\n")
@@ -81,7 +139,14 @@
 ;; == set ===
 
 (define (set lst)
-  	(display "Not yet implemented\n")
+  	(remove-duplicates
+		(filter
+			(lambda (element)
+				(number? element)
+			)
+			lst
+		)
+	)
 )
 
 (display "=== set ===\n")
@@ -90,7 +155,7 @@
 (set '(10 (a b 3) 4 (8) c d (a b 3) d c 11)) ;; '(10 4 11)
 
 (define (union x y)
-  	(display "Not yet implemented\n")
+  	(set (append x y))
 )
 
 (display "=== union ===\n")
@@ -99,7 +164,14 @@
 (union '(2 a 8 4) '(b c d)) ;; '(2 8 4)
 
 (define (intersection x y)
-  	(display "Not yet implemented\n")
+	(set
+		(filter
+			(lambda (item)
+				(member item x)
+			)
+			y
+		)
+	)
 )
 
 (display "=== intersection ===\n")
